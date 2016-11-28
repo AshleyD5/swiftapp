c8ba781fa8c23128daf71aca259cafe2496f5dcb
Beta <ashleycaroldavenport@gmail.com>
2016-11-28 06:01:58 -0500

    My First Commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -2,14 +2,27 @@
 //  ViewController.swift
 //  Swift App
 //
-//  Created by Beta on 11/27/16.
-//  Copyright © 2016 Beta. All rights reserved.
+//  Created by Ashley Davenport on 11/27/16.
+//  Copyright © 2016 Ashley Davenport. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+    
+        tapCount = tapCount + 1
+     
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times"
+    }
+   
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
